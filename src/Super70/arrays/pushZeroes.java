533fd327759d8ee0ec6d80af74ddeeeb0a66fc00
Farhantigadi <farhantigadi123@gmail.com>
2025-04-17 16:07:01 +0530

    array -> code updates

--- src/Super70/arrays/pushZeroes.java
+++ src/Super70/arrays/pushZeroes.java
@@ -22,3 +22,25 @@ class pushZeroes{
         System.out.println(Arrays.toString(result));
     }
 }
+
+
+ class PushZeroes2 {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 0, 4, 0, 1};
+        int index = 0;
+
+        // Move non-zeroes forward
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index++] = arr[i];
+            }
+        }
+
+        // Fill remaining with zeroes
+        while (index < arr.length) {
+            arr[index++] = 0;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
