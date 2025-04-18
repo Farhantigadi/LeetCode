a77ce7ae6bd628578de3f8db076d842aee8944eb
Farhantigadi <farhantigadi123@gmail.com>
2025-04-18 11:38:20 +0530

    String -> pallindrome leetcode

---
+++ src/Strings.java
@@ -0,0 +1,21 @@
+public class Strings {
+    public static void main(String[] args) {
+        boolean result = issPallindrome("A man, a plan, a canal: Panama");
+        System.out.println(result);
+    }
+    static boolean issPallindrome(String s){
+        s = s.replaceAll("[^A-Za-z0-9]","").toLowerCase();
+        int left =0;
+        int right =s.length()-1;
+        while (left<right){
+            if (s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+
+
+    }
+}
